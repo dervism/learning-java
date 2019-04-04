eb8aa8df3bb1e0b09411db6df5c8ef0d9d0deee8
Dervis Mansuroglu <dervis4@gmail.com>
2019-04-04 12:25:19 +0200

    Update Calculator.java

--- labs/src/main/java/no/dervis/learningjava/labs/basics/calculator/Calculator.java
+++ labs/src/main/java/no/dervis/learningjava/labs/basics/calculator/Calculator.java
@@ -2,44 +2,18 @@ package no.dervis.learningjava.labs.basics.calculator;
 
 public class Calculator {
 
-    private int multiply;
-
-    private String type;
-
-
-    public Calculator(String type) {
-        this.type = type;
-        this.multiply = 2;
+    public Calculator() {
+        
     }
 
 
-    public int addNumbers(int a, int b) {
-        return a+b;
+    public int sum(int a, int b) {
+        return a + b;
     }
 
 
-    public int multiplyNumbers(int i, int j) {
-        return i*j;
-    }
-
-
-    public String changeModel(String model) {
-        return getType() + " " + model;
-    }
-
-
-
-    public void setMultiply(int multiply) {
-        this.multiply = multiply;
-    }
-
-    public int getMultiply(){
-        return multiply;
-    }
-
-
-    public String getType() {
-        return type;
+    public int multiply(int i, int j) {
+        return i * j;
     }
 
 
