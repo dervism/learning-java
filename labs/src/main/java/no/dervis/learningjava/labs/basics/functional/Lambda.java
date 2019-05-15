e87b3188f97de08661dc7b10939a37f48bfb4c07
dervism <mail@dervis.no>
2019-05-15 10:30:16 +0200

    Adding Lambda lab.

---
+++ labs/src/main/java/no/dervis/learningjava/labs/basics/functional/Lambda.java
@@ -0,0 +1,55 @@
+package no.dervis.learningjava.labs.basics.functional;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Lambda {
+
+    class Employee {
+        private int salary;
+        public Employee(int salary) { this.salary = salary; }
+        public String toString() { return "Employee{ salary=" + salary + '}'; }
+    }
+
+    // lambda with a closure
+    Predicate<Employee> paidMore(int amount) {
+        return employee -> employee.salary > amount;
+    }
+
+    // lambda with a closure
+    List<Employee> highPaidLambdaClosure(List<Employee> emps) {
+        int threshold = 150;
+
+        return emps.stream()
+                .filter(e -> e.salary > threshold)
+                .collect(Collectors.toList());
+    }
+
+    // only lambda
+    List<Employee> highPaidLambda(List<Employee> emps) {
+        return emps.stream()
+                .filter(e -> e.salary > 150)
+                .collect(Collectors.toList());
+    }
+
+    public void testLambdas() {
+
+        Employee employeeHighPaid = new Employee(200);
+        Employee employeeLowPaid = new Employee(100);
+
+        Predicate<Employee> isPaidHigh = paidMore(150);
+
+        System.out.println("Is paid high: " + isPaidHigh.test(employeeHighPaid));
+        System.out.println("Is paid high: " + isPaidHigh.test(employeeLowPaid));
+
+        System.out.println(highPaidLambdaClosure(List.of(employeeHighPaid, employeeLowPaid)));
+
+    }
+
+
+    public static void main(String[] args) {
+        new Lambda().testLambdas();
+    }
+
+}
